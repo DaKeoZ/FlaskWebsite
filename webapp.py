d5d2d0fd28373c92c12bffb32c0b6a644d1318cc
DaKeoZ <aurelien.farges@gmail.com>
2021-11-03 00:42:21 +0000

    Added the home summary and the CV page that shows a pdf file :rocket:

--- webapp.py
+++ webapp.py
@@ -5,8 +5,23 @@ webapp = Flask(__name__)
 
 @webapp.get("/")
 def home():
-    return render_template("home.html",
-                           title="Welcome")
+    return render_template("home.html", title="Welcome")
+
+@webapp.get("/about")
+def about():
+    return render_template("about.html", title="About me")
+
+@webapp.get("/cv")
+def mycv():
+    return render_template("cv.html", title="My CV")
+
+@webapp.get("/technologies")
+def mycv():
+    return render_template("technologies.html", title="Computing technologies")
+
+@webapp.get("/interests")
+def mycv():
+    return render_template("interests.html", title="Personal interests")
 
 
 if __name__ == "__main__":
