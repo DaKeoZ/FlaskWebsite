fedc0a782e0feb35daae96629806749f5afaa36d
DaKeoZ <aurelien.farges@gmail.com>
2021-11-03 02:08:24 +0000

    Done the technologies page :rocket:

--- webapp.py
+++ webapp.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 webapp = Flask(__name__)
 numTechnologies = 0
+currentTechnology = 1
 
 @webapp.get("/")
 def home():
@@ -18,18 +19,29 @@ def mycv():
 
 @webapp.get("/technologies")
 def defaultTechnology():
-    return technologies(1)
+    '''return technologies(1)'''
+    return render_template("technologies.html", title="Computing technologies")
 
 @webapp.get("/technologies/<index>")
 def technologies(index):
+    '''currentTechnology = int(index)
     if (int(index) > numTechnologies): index = numTechnologies
     s = "technologies{id}.html"
-    return render_template(s.format(id=index), title="Computing technologies")
+    return render_template(s.format(id=index), title="Computing technologies")'''
+    return render_template("technologies.html", title="Computing technologies")
 
 @webapp.get("/interests")
 def interests():
     return render_template("interests.html", title="Personal interests")
 
+
+'''def previousTechnology():
+    return
+
+@webapp.post("/next")
+def nextTechnology():
+    s = "technologies{id}.html"
+    return render_template(s.format(id=currentTechnology+1), title="Computing technologies")'''
 
 if __name__ == "__main__":
     files = os.listdir("./templates")
